7e4b045d3bfe4602c9d1b46248a2d11b7a9ce6d2
yairctt <yaircruztorres@gmail.com>
2026-04-06 21:41:24 -0600

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/web-giang-aluminum/',
   build: {
     rollupOptions: {
       input: {
